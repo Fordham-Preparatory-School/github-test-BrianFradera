06efeb8d2ded4198e5c87ed051fd6c6ab4081746
BrianFradera <fraderab26@fordhamprep.org>
2024-11-05 18:00:10 -0500

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+Name: Brian Fradera Jr.
+Date: 9/9/2024
+*/
+
+public class HelloWorld //class declaration
+
+{
+    public static void main(String[] args)//main method
+    {
+        System.out.println("Hello World!");//printed statement
+    }
+}
